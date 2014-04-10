f0d0c4967ae490843812832a021badbffb6bc989
yangjing911 <yj911911@163.com>
2014-04-10 18:24:05 +0800

    Initial Commit

---
+++ Hello_World/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  Hello_WorldAppController.h
+//  Hello_World
+//
+//  Created by kaffee on 14-4-10.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ Hello_World/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  Hello_WorldAppController.h
+//  Hello_World
+//
+//  Created by kaffee on 14-4-10.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
